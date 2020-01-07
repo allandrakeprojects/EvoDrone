957ba9341d5f93a43423f3ffb9d2467b5d46bbe2
Allan Drake Paladin Dulay <40012648+allandrakeprojects@users.noreply.github.com>
2020-01-07 17:18:30 +0800

    Add Main Menu UI

---
+++ Assets/EvoDrone/Scripts/Custom/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadMainMenuAfterDelay()
+    {
+        Invoke("MainMenu", 1.0f);
+    }
+
+    public void LoadGameAfterDelay()
+    {
+        Invoke("Game", 1.0f);
+    }
+
+    public void LoadQuitAfterDelay()
+    {
+        Invoke("Quit", 1.0f);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/EvoDrone/Scripts/Custom/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private LevelManager levelManager;
+
+    void Start()
+    {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+    }
+
+    public void PLAYGAME()
+    {
+        levelManager.LoadGameAfterDelay();
+    }
+
+    public void OpenMainMenu()
+    {
+        levelManager.LoadMainMenuAfterDelay();
+    }
+
+    public void Quit()
+    {
+        levelManager.LoadQuitAfterDelay();
+    }
+}
