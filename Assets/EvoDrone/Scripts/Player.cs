ea85ecbbd744b24df5b0ceb0753bec12c630ec7c
Allan Drake Paladin Dulay <40012648+allandrakeprojects@users.noreply.github.com>
2020-01-08 09:08:56 +0800

    Revert "Add GameOver"
    This reverts commit da047ffae1292a837f6a3dcc422e897e727e57de.

--- Assets/EvoDrone/Scripts/Player.cs
+++ Assets/EvoDrone/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,15 +10,8 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public GameObject destructionFX;
-    public GameObject Score_Count;
-    public GameObject Score_Text;
 
-    public GameObject GameOver_Header;
-    public GameObject GameOver;
-
-    public static Player instance;
-    private static Text score;
-    public Text scoreText;
+    public static Player instance; 
 
     private void Awake()
     {
@@ -26,37 +20,10 @@ public class Player : MonoBehaviour
     }
 
     //method for damage proceccing by 'Player'
-    public void GetDamage(int damage)
+    public void GetDamage(int damage)   
     {
         Destruction();
-        ShowDeathScreen();
-    }
-
-    public void ShowDeathScreen()
-    {
-        score = GameObject.Find("Score_Count").GetComponentInChildren<Text>();
-        int highscore = PlayerPrefs.GetInt("highscore");
-        int getCurrentScore = PlayerPrefs.GetInt("score");
-        if (getCurrentScore > highscore)
-        {
-            scoreText.text = "NEW BEST";
-            PlayerPrefs.SetInt("highscore", getCurrentScore);
-            PlayerPrefs.Save();
-        }
-        Score_Count.SetActive(false);
-        Score_Text.SetActive(false);
-
-        Text scoreGameOver = GameOver.GetComponentInChildren<Text>();
-        scoreGameOver.text = score.text;
-        GameOver_Header.SetActive(true);
-        GameOver.SetActive(true);
-
-        //StartCoroutine(ShowGameOverCoroutine());
-    }
-    IEnumerator ShowGameOverCoroutine()
-    {
-        yield return new WaitForSeconds(2.0f);
-    }
+    }    
 
     //'Player's' destruction procedure
     void Destruction()
