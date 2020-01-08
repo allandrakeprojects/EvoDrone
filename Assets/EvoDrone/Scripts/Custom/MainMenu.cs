bd1a98b8e6cc051bffaff9b2ae882f7128444ccb
Allan Drake Paladin Dulay <40012648+allandrakeprojects@users.noreply.github.com>
2020-01-08 12:31:51 +0800

    Add PowerUps

--- Assets/EvoDrone/Scripts/Custom/MainMenu.cs
+++ Assets/EvoDrone/Scripts/Custom/MainMenu.cs
@@ -13,13 +13,24 @@ public class MainMenu : MonoBehaviour
     public Text ClaimWaitMinutes;
     bool detectStartCountdown = false;
 
+    public Text FirerateLvl;
+    public Button FirerateButton;
+    public Text FirerateCoin;
+
+    public Text FirepowerLvl;
+    public Button FirepowerButton;
+    public Text FirepowerCoin;
+
     void Start()
     {
         ShowCoin();
         ShowBest();
         CheckCoin();
+        Powerups();
+
         PlayerPrefs.SetInt("score", 0);
         PlayerPrefs.Save();
+
         levelManager = GameObject.FindObjectOfType<LevelManager>();
     }
 
@@ -83,13 +94,22 @@ public class MainMenu : MonoBehaviour
         }
     }
 
-    public void AddCoin(int num)
+    public void AddMinusCoin(int num, int type)
     {
         int coin = PlayerPrefs.GetInt("coin");
-        coin += num;
+        if (type == 0)
+        {
+            coin += num;
+        }
+        else
+        {
+            coin -= num;
+        }
         PlayerPrefs.SetInt("coin", coin);
         PlayerPrefs.Save();
         Coin.text = FormatCoin(coin);
+        
+        checkPowerups();
     }
 
     static string FormatCoin(int num)
@@ -109,7 +129,7 @@ public class MainMenu : MonoBehaviour
     public void ClaimCoin()
     {
         ClaimCoinButton.interactable = false;
-        AddCoin(100);
+        AddMinusCoin(100, 0);
         PlayerPrefs.SetString("waitMinutes", System.DateTime.Now.AddMinutes(5).ToString());
         PlayerPrefs.Save();
         detectStartCountdown = true;
@@ -117,21 +137,28 @@ public class MainMenu : MonoBehaviour
 
     public void CheckCoin()
     {
-        DateTime datevalue1 = DateTime.Parse(PlayerPrefs.GetString("waitMinutes"));
-        DateTime datevalue2 = DateTime.Now;
-        TimeSpan timeDifference = datevalue1 - datevalue2;
-        string time = new DateTime(timeDifference.Ticks).ToString("mm:ss");
-        ClaimWaitMinutes.text = time;
+        try
+        {
+            DateTime datevalue1 = DateTime.Parse(PlayerPrefs.GetString("waitMinutes"));
+            DateTime datevalue2 = DateTime.Now;
+            TimeSpan timeDifference = datevalue1 - datevalue2;
+            string time = new DateTime(timeDifference.Ticks).ToString("mm:ss");
+            ClaimWaitMinutes.text = time;
 
-        if (int.Parse(time.Replace(":", "")) <= 0000)
-        {
-            ClaimCoinButton.interactable = true;
-            detectStartCountdown = false;
-            ClaimWaitMinutes.text = "Collect";
+            if (int.Parse(time.Replace(":", "")) <= 0000)
+            {
+                ClaimCoinButton.interactable = true;
+                detectStartCountdown = false;
+                ClaimWaitMinutes.text = "Collect";
+            }
+            else
+            {
+                detectStartCountdown = true;
+            }
         }
-        else
+        catch (Exception err)
         {
-            detectStartCountdown = true;
+            // leave blank
         }
     }
 
@@ -163,5 +190,163 @@ public class MainMenu : MonoBehaviour
         {
             // leave blank
         }
+    }
+
+
+    public void UpgradeFirerate()
+    {
+        int lvl = PlayerPrefs.GetInt("firerate_lvl");
+        lvl += 1;
+        PlayerPrefs.SetInt("firerate_lvl", lvl);
+        PlayerPrefs.Save();
+        
+        AddMinusCoin( int.Parse(DecryptCoin(FirerateCoin.text)), 1);
+        Powerups();
+    }
+    public void UpgradeFirepower()
+    {
+        int lvl = PlayerPrefs.GetInt("firepower_lvl");
+        lvl += 1;
+        PlayerPrefs.SetInt("firepower_lvl", lvl);
+        PlayerPrefs.Save();
+
+        AddMinusCoin(int.Parse(DecryptCoin(FirepowerCoin.text)), 1);
+        Powerups();
+    }
+
+
+
+
+    public void Powerups()
+    {
+        firerate();
+        firepower();
+        checkPowerups();
+    }
+
+    public void firerate()
+    {
+        try
+        {
+            float DEFAULT_VAL = 3.3f;
+            float LOOP_VAL = .2f;
+            int DEFAULT_COIN = 500;
+            int LOOP_COIN = 500;
+            if (PlayerPrefs.GetInt("firerate_lvl", 1) == 1)
+            {
+
+                PlayerPrefs.SetInt("firerate_lvl", 1);
+                PlayerPrefs.Save();
+            }
+            else
+            {
+                int lvl = PlayerPrefs.GetInt("firerate_lvl");
+                float new_default_val = (lvl * LOOP_VAL) + DEFAULT_VAL;
+                int new_default_coin = (lvl * LOOP_COIN) + DEFAULT_COIN;
+
+                FirerateLvl.text = "FIRERATE [LV" + lvl + "]";
+                FirerateCoin.text = FormatCoin(new_default_coin);
+            }
+        }
+        catch (Exception err)
+        {
+            // leave blank
+        }
+    }
+
+    public void firepower()
+    {
+        try
+        {
+            int DEFAULT_VAL = 0;
+            int LOOP_VAL = 1;
+            int DEFAULT_COIN = 0;
+            int LOOP_COIN = 7500;
+            if (PlayerPrefs.GetInt("firepower_lvl", 1) == 1)
+            {
+
+                PlayerPrefs.SetInt("firepower_lvl", 1);
+                PlayerPrefs.Save();
+            }
+            else
+            {
+                int lvl = PlayerPrefs.GetInt("firepower_lvl");
+                if (lvl == 3)
+                {
+                    FirepowerLvl.text = "FIREPOWER [LV3]";
+                    FirepowerButton.interactable = false;
+                    FirepowerCoin.text = "MAX";
+                }
+                else
+                {
+                    int new_default_val = (lvl * LOOP_VAL) + DEFAULT_VAL;
+                    int new_default_coin = (lvl * LOOP_COIN) + DEFAULT_COIN;
+
+                    Debug.Log(new_default_val);
+                    Debug.Log(new_default_coin);
+
+                    FirepowerLvl.text = "FIREPOWER [LV" + lvl + "]";
+                    FirepowerCoin.text = FormatCoin(new_default_coin);
+                }
+            }
+        }
+        catch (Exception err)
+        {
+            // leave blank
+        }
+    }
+
+    public void checkPowerups()
+    {
+        try
+        {
+            float coin = float.Parse(DecryptCoin(Coin.text));
+
+            float firerateCoin = float.Parse(DecryptCoin(FirerateCoin.text));
+            if (firerateCoin > coin)
+            {
+                FirerateButton.interactable = false;
+            }
+            else
+            {
+                FirerateButton.interactable = true;
+            }
+
+            if (FirepowerCoin.text == "MAX")
+            {
+                return;
+            }
+            
+            float firepowerCoin = float.Parse(DecryptCoin(FirepowerCoin.text));
+            if (firepowerCoin > coin)
+            {
+                FirepowerButton.interactable = false;
+            }
+            else
+            {
+                FirepowerButton.interactable = true;
+            }
+        }
+        catch (Exception err)
+        {
+            // leave blank
+        }
+    }
+
+
+    static string DecryptCoin(string num)
+    {
+        num = num.ToUpper();
+
+        if (num.Contains("K") && num.Contains("."))
+        {
+            num = num.Replace(".", "").Replace("K", "00");
+        }
+        else if (num.Contains("K"))
+        {
+            num = num.Replace("K", "000");
+        }
+
+        return num;
     }
 }
